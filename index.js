f5bc7e6717b3d759b0425625d82ac6f6d2eba61b
nathanngl <nathan.nainggolan15@gmail.com>
2024-01-13 18:00:50 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import { createServer } from 'http';
+import staticHandler from 'serve-handler';
+import ws, { WebSocketServer } from 'ws';
+
+const server = createServer((req, res) => {
+    return staticHandler(req, res, { public: 'public' })
+});
+
+const wss = new WebSocketServer({ server })
+
+wss.on('connection', (client) => {
+    console.log('Client connected !')
+    client.on('message', (msg) => {
+        console.log(`Message:${msg}`);
+        broadcast(msg)
+    })
+})
+function broadcast(msg) {
+    for (const client of wss.clients) {
+        if (client.readyState === ws.OPEN) {
+            client.send(msg)
+        }
+    }
+}
+server.listen(process.argv[2] || 3000, () => {
+    console.log(`server listening...`);
+})
